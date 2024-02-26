256be95186511da7627cb9ac05969ccca0c576e4
navidmirzad <navi0221@stud.kea.dk>
2024-02-26 14:11:31 +0100

    so far so good

--- View/script.js
+++ View/script.js
@@ -2,10 +2,20 @@
 
 window.addEventListener("load", start);
 
+// *************** CONTROLLER ***************
+
 function start() {
     console.log("JavaScript kører");
     makeBoardClickable();
 }
+
+function selectCell(row, col) {
+    writeToCell(row, col, 1);
+    displayBoard();
+}
+
+
+// *************** VIEW *********************
 
 function makeBoardClickable() {
     document.querySelector("#board")
@@ -18,7 +28,41 @@ function boardClicked(event) {
     const col = cell.dataset.col;
 
     console.log(`Clicked on row: ${row} col: ${col}`);
+    selectCell(row, col);
 }
 
+function displayBoard() {
+    for (let row = 0; row < 3; row++) {
+        for (let col = 0; col < 3; col++) {
+            const value = readFromCell(row, col);
+            const cell = document
+            .querySelector(`[data-row="${row}"][data-col="${col}"]`);
+            cell.textContent = value;
+
+            switch(value) {
+                case 0: cell.textContent = " "; break;
+                case 1: cell.textContent = "X"; break;
+                case 2: cell.textContent = "O"; break;
+            }
+
+        }
+    }
+}
+
+// ****************** MODEL *********************
+
+const model = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0],
+];
 
 
+
+function writeToCell(row, col, value) {
+    model[row][col] = value;
+}
+
+function readFromCell(row, col) {
+    return model[row][col];
+}
